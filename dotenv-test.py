26c2d90c49aadbfbef07995ff62c500cdebbc3ed
williserdman <willis.erdman@gmail.com>
2024-06-16 10:30:17 -0700

    added my own .env variables

--- dotenv-test.py
+++ dotenv-test.py
@@ -1,8 +1,7 @@
 from os import environ as env
 from dotenv import load_dotenv
 
-load_dotenv()
-
 print('CLIENT_ID:  {}'.format(env['CLIENT_ID']))
 print('CLIENT_SECRET: {}'.format(env['CLIENT_SECRET']))
 print('USER_AGENT:     {}'.format(env['USER_AGENT']))
+print("ELEVENLABS_API_KEY: {}".format(env['ELEVENLABS_API_KEY']))
